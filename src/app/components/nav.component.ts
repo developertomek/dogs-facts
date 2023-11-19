42cc5b20f7341d2d38082a44d446d4028ae61717
developertomek <tomekcodes@gmail.com>
2023-11-19 21:37:50 +0100

    add styles

--- src/app/components/nav.component.ts
+++ src/app/components/nav.component.ts
@@ -6,8 +6,12 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <div class="flex justify-between max-w-960 m-auto h-16 items-center px-8">
-      <a routerLink="/" class="hover:cursor-pointer font-semibold">Doggo</a>
-      <button>
+      <a
+        routerLink="/"
+        class="hover:cursor-pointer hover:scale-105 transition-all font-semibold text-green-400"
+        >Doggo</a
+      >
+      <button class="hover:scale-105 transition-all">
         <a routerLink="/" class="py-2 px-8 bg-green-400 rounded-md"
           >Want more?</a
         >
