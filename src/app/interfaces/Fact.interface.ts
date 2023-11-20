65cd7320b803cc3391db0d9617eb203bb78a3cc2
developertomek <tomekcodes@gmail.com>
2023-11-20 18:48:19 +0100

    add api

--- src/app/interfaces/Fact.interface.ts
+++ src/app/interfaces/Fact.interface.ts
@@ -2,3 +2,8 @@ export interface Fact {
   facts: string[];
   success: boolean;
 }
+
+export interface DogImg {
+  message: string;
+  status: string;
+}
--- src/app/services/facts.service.ts
+++ src/app/services/facts.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Fact } from '../interfaces/Fact.interface';
+import { Observable, combineLatest, shareReplay } from 'rxjs';
+import { DogImg, Fact } from '../interfaces/Fact.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +9,20 @@ import { Fact } from '../interfaces/Fact.interface';
 export class FactsService {
   constructor(private http: HttpClient) {}
 
-  API_URL = 'https://dog-api.kinduff.com/api/facts';
+  API_FACTS_URL = 'https://dog-api.kinduff.com/api/facts';
+  API_IMG_URL = ' https://dog.ceo/api/breeds/image/random';
 
-  getOne(): Observable<Fact> {
-    return this.http.get<Fact>(this.API_URL + '?number=1');
+  getOne(): Observable<[Fact, DogImg]> {
+    return combineLatest([this.getFacts(1), this.getRandomImg(1)]).pipe(
+      shareReplay()
+    );
+  }
+
+  private getFacts(num: number): Observable<Fact> {
+    return this.http.get<Fact>(`${this.API_FACTS_URL}?number=${num}`);
+  }
+
+  private getRandomImg(num: number): Observable<DogImg> {
+    return this.http.get<DogImg>(`${this.API_IMG_URL}?number=${num}`);
   }
 }
